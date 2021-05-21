efbb26888afc973c2814b1909cc9ddb565b2eb55
Brandon Hill <brandonjon26@gmail.com>
2021-05-21 11:40:24 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+Console.Write("First Name> ");
+string firstName = Console.ReadLine();
+
+Console.Write("Last Name> ");
+string lastName = Console.ReadLine();
+
+Console.WriteLine($"{lastName} {firstName}");
+
+string reverse = string.Empty;
+string fullName = firstName + " " + lastName;
+
+for (int i = fullName.Length - 1; i >= 0; i--)
+{
+    reverse += fullName[i];
+}
+
+Console.WriteLine(reverse);
+
+/*
+Console.Write("Who would you like to say hello to? ");
+
+string name = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(name))
+{
+    Console.WriteLine("Fine, don't say 'hello'!");
+}
+else
+{
+    Console.WriteLine($"Hello, {name}!");
+}
+*/
